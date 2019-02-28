1c35364b2b11a351b0cc544c796d3107567a369f
Alex Sexton <alex@threefour.co.uk>
2019-02-28 16:31:12 +0000

    Refactor entry-meta.php

--- partials/entry-meta.php
+++ partials/entry-meta.php
@@ -1,4 +1,14 @@
-<p class="byline author vcard">
-  <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author"><?php echo get_the_author(); ?></a>,
-  <time class="published" datetime="<?php echo get_the_time('Y-m-d'); ?>"><?php echo get_the_date('j F Y'); ?></time> &mdash; <?php echo get_the_category_list(', ') ?>
-</p>
+<div class="byline author vcard">
+
+  <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author"><?php echo get_the_author(); ?></a>
+
+  <time class="published" datetime="<?php echo get_the_time('Y-m-d'); ?>"><?php echo get_the_date('j F Y'); ?></time>
+
+  <?php $terms = wp_get_object_terms( $post->ID, 'category' ); ?>
+  <?php foreach ($terms as $term) : ?>
+    <?php if ( ($term->slug !== 'uncategorised') && ($term->slug !== 'uncategorized')) : ?>
+        <li><a href="/<?php echo $term->taxonomy.'/'.$term->slug ?>"><?php echo $term->name; ?></a></li>
+    <?php endif; ?>
+  <?php endforeach; ?>
+
+</div>
