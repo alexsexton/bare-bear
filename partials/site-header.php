401d41a5e32c3e5be3237600d5c01dff93cfa331
Alex Sexton <alex@threefour.co.uk>
2019-03-05 15:18:17 +0000

    Partial search box

--- partials/site-header.php
+++ partials/site-header.php
@@ -22,15 +22,7 @@
     ));
     ?>
   </nav>
-
-  <form class="site-search" method="get" action="/">
-    <fieldset class="form-group">
-      <label for="s"><?php esc_html_e( 'What are you looking for?', 'bare' ); ?></label>
-      <input type="search" value="<?php echo get_search_query(); ?>" name="s" />
-      <button class="button" type="submit"><?php esc_html_e( 'Search', 'bare' ); ?></button>
-    </fieldset>
-  </form>
-
-
+  
+  <?php get_template_part('partials/site-search'); ?>
 
 </header>
