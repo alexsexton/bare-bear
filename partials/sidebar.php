a11193f18f517071aa1a903cfd1fce1878b7e811
Alex Sexton <alex@threefour.co.uk>
2019-03-05 15:18:01 +0000

    Added ul around list-categories and headings are translatable

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,17 +1,19 @@
 <aside class="sidebar" role="complementary">
   <div class="categories">
-    <h3>Categories</h3>
-    <?php wp_list_categories( array(
-      'orderby' => 'name',
-      'title_li' => '',
-    )); ?>
+    <h3><?php esc_html_e( 'Categories', 'bare' ); ?></h3>
+    <ul>
+      <?php wp_list_categories( array(
+        'orderby' => 'name',
+        'title_li' => '',
+      )); ?>
+    </ul>
   </div>
 
   <?php if (get_the_tags()) : ?>
     <div class="tags">
-      <h3>Tags</h3>
+      <h3><?php esc_html_e( 'Tags', 'bare' ); ?></h3>
       <p><?php the_tags('',', ','') ?></p>
     </div>
   <?php endif ?>
-  
+
 </aside>
